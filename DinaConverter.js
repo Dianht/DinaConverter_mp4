d6e7d8871fba75c55c44b041d28c49ba296bced2
Dianht <dinath.sh@outlook.fr>
2021-09-12 12:38:25 +0200

    update

---
+++ DinaConverter.js
@@ -0,0 +1,32 @@
+// Marche pas vu que c'est du NodeJs
+const express = require("express");
+const app = express();
+const fs = require('fs');
+const ytdl = require('ytdl-core')
+
+app.use(express.json());
+app.use(express.static("public"));
+
+app.get("/", function(request, response) {
+    response.sendFile(__dirname + "/index.html");
+});
+
+app.get("/style.css", function(request, response) {
+    response.sendFile(__dirname + "/style.css");
+});
+
+app.get("/Images/Background.jpg", function(request, response) {
+    response.sendFile(__dirname + "/Images/Background.jpg");
+});
+
+app.get("/script.js", function(request, response) {
+    response.sendFile(__dirname + "/script.js");
+});
+
+app.get("/download", async function(request, response) {
+    //Retrieves the link and sends the ydtl module
+    const videoURL = request.query.videoURL;
+    ytdl(videoURL).pipe(fs.createWriteStream('video.mp4'));
+});
+
+app.listen(3000);
--- script.js
+++ script.js
@@ -1,28 +1,5 @@
-// Marche pas vu que c'est du NodeJs
-const express = require("express");
-const app = express();
-const fs = require('fs');
-const ytdl = require('ytdl-core')
-
-app.use(express.json());
-app.use(express.static("public"));
-
-app.get("/",function(request,response){
-  response.sendFile(__dirname + "/index.html");
+const host = "http://" + window.location.host + "/";
+document.querySelector("#btn").addEventListener("click", function() {
+    let videoUrl = document.querySelector("#videoUrl").value;
+    window.open(host + "download?videoURL=" + videoUrl);
 });
-
-app.get("/style.css",function(request,response){
-  response.sendFile(__dirname + "/style.css");
-});
-
-app.get("/Images/Background.jpg",function(request,response){
-  response.sendFile(__dirname + "/Images/Background.jpg");
-});
-
-app.get("/download",async function(request,response){
-	//I put a link to see if at the opening of the page the programm download the video 
- ytdl("https://www.youtube.com/watch?v=OCZ5ISW75E8")
-  .pipe(fs.createWriteStream('video.mp4'));
-});
-
-app.listen(5000);
