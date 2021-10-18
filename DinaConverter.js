85b50953619c3e3d19cab844a6605cf07faa06e1
Dianht <dinath.sh@outlook.fr>
2021-10-18 21:40:20 +0200

    update

--- DinaConverter.js
+++ DinaConverter.js
@@ -25,10 +25,9 @@ app.get("/script.js", function(request, response) {
 app.get("/download", async function(request, response) {
     //Retrieves the link and sends the ydtl module
     const videoURL = request.query.videoURL;
-    ytdl('http://www.youtube.com/watch?v=aqz-KE-bpKQ').pipe(fs.createWriteStream('video.mp4'));
+    ytdl(videoURL).pipe(fs.createWriteStream("video.mp4"));
 });
 
 app.listen((process.env.PORT || 5000), function(){
     console.log('listening on *:5000');
   });
-  
